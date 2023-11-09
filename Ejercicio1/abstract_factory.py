f03b3e62ea3318f2accacb6309e0a731d843b8e3
Agirre8 <inagi30@gmail.com>
2023-11-09 09:51:55 +0100

    1 acabado

--- Ejercicio1/abstract_factory.py
+++ Ejercicio1/abstract_factory.py
@@ -10,7 +10,6 @@ class DataFactory(ABC):
     def create_data_visualization(self):
         pass
 
-# Clase concreta de fábrica para análisis estadísticos
 class StatisticalAnalysisFactory(DataFactory):
     def create_statistical_analysis(self):
         return MeanAnalysis()  # Puedes cambiar esto a otras clases concretas según necesites
@@ -62,25 +61,38 @@ class HistogramVisualization(DataVisualization):
         plt.ylabel('Frecuencia')
         plt.title('Histograma')
         plt.show()
-# Ejemplo de uso
+
 def generate_report(factory, data):
-    statistical_analysis = factory.create_statistical_analysis()
+    # Crear instancias de los análisis estadísticos
+    mean_analysis = MeanAnalysis()
+    mode_analysis = ModeAnalysis()
+    median_analysis = MedianAnalysis()
+
+    # Realizar análisis estadísticos
+    result_mean = mean_analysis.perform_analysis(data)
+    result_mode = mode_analysis.perform_analysis(data)
+    result_median = median_analysis.perform_analysis(data)
+
+    # Imprimir resultados
+    print("Resultado del análisis de media:", result_mean)
+    print("Resultado del análisis de moda:", result_mode)
+    print("Resultado del análisis de mediana:", result_median)
+
     data_visualization = factory.create_data_visualization()
-    
-    if statistical_analysis:
-        result = statistical_analysis.perform_analysis(data)
-        print("Resultado del análisis estadístico:", result)
-    
+
     if data_visualization:
         data_visualization.create_chart(data)
+
+
+
 
 if __name__ == "__main__":
     import pandas as pd
     datos = pd.read_csv('Ejercicio1/dataset_limpio.csv', delimiter = ",")
     hora = datos["Hora Intervención"]
 
-    statistical_factory = StatisticalAnalysisFactory()
-    generate_report(statistical_factory, hora)
-    
+    factory = StatisticalAnalysisFactory()
+    generate_report(factory, hora)
+
     visualization_factory = DataVisualizationFactory()
     generate_report(visualization_factory, hora)
